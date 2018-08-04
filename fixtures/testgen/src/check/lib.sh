b0102070cf5b78e5bbea7e82401b9ad09cabf071
Christoffer Ventus <christoffer.ventus@futurice.com>
2018-08-04 23:08:37 +0300

    Make test definition rules more ergonomic
    Add new functions for generating targets which might add an output
    directory prefix to the target name and code-generation or linkage
    dependencies to the target by default. These functions follow the
    pattern
    
        build_<type>_target
    
    Similarly, add more fine grained functions for compiling objects which
    have the pattern
    
        build_<cmd>_recipe
    
    e.g. build_ld_pattern for defining commands for linking an item which
    adds library search paths and module link dependencies automatically.
    
    Also define the variables
    
    - MODULE_TEMP_DIR
    - TEST_TEMP_DIR
    - TEST_BIN_DIR
    
    during the evaluation of a module configuration.

--- fixtures/testgen/src/check/lib.sh
+++ fixtures/testgen/src/check/lib.sh
@@ -1,62 +1,56 @@
-# Define NM if it wasn't
+# Make NM available from environment by default
 export NM=${NM:-nm}
 
 # Define a test with an auto-generated main function which
 # calls all functions named "test_*"
-# CHECK [<name>] <source>.c [CC options]
+# check [<name>] <source>.c [CC options]
 CHECK()
 {
-  CHECK_SOURCE="$BUILD_SOURCE_DIR/$MODULE/$1"
+  CHECK_SOURCE="$MODULE_DIR/$1"
   CHECK_NAME=$(basename "$CHECK_SOURCE" .c)
   case "$2" in
   (-*) ;;
-  (?*.c) CHECK_SOURCE="$BUILD_SOURCE_DIR/$MODULE/$2"; shift;;
+  (?*.c) CHECK_SOURCE="$MODULE_DIR/$2"; shift;;
   esac
   shift
-  CHECK_MKFILE="$TARGET_TEMP_DIR/$MODULE/test/$CHECK_NAME.d"
-  CHECK_OBJECT="$TARGET_TEMP_DIR/$MODULE/test/$CHECK_NAME.o"
-  CHECK_GENSRC="$TARGET_TEMP_DIR/$MODULE/test/${CHECK_NAME}_check.c"
-  CHECK_GENOBJ="$TARGET_TEMP_DIR/$MODULE/test/${CHECK_NAME}_check.o"
-  CHECK_BINARY="$TARGET_TEST_BINARY_DIR/$MODULE/$CHECK_NAME"
+  CHECK_OBJECT="$TEST_TEMP_DIR/$CHECK_NAME.o"
+  CHECK_GENSRC="$TEST_TEMP_DIR/${CHECK_NAME}_check.c"
+  CHECK_GENOBJ="$TEST_TEMP_DIR/${CHECK_NAME}_check.o"
+  CHECK_BINARY="$TEST_BIN_DIR/$CHECK_NAME"
 
-  printf '%s: %s %s %s\n' \
+  TEST "$CHECK_BINARY"
+
+  build_bin_target \
     "$CHECK_BINARY" \
     "$CHECK_SOURCE" \
-    "$TARGET_LIBRARY_DIR/lib$MODULE.a" \
+    "$LIBRARY" \
     "$TARGET_LIBRARY_DIR/libcheck.a"
 
-  # Build object file of tests
-  build_source_recipe "$@" \
-    $(build_depend_flags "$CHECK_BINARY" "$CHECK_MKFILE") \
-    -c "\"$CHECK_SOURCE\"" \
-    -o "\"$CHECK_OBJECT\""
-
-  # Test dependency information
-  printf '\t$(AR) -rc "%s" "%s"\n' "$TEST_DEPEND" "$CHECK_MKFILE"
+  build_object_recipe \
+    "$CHECK_BINARY" \
+    "$TEST_DEPEND" \
+    "$CHECK_OBJECT" \
+    "$CHECK_SOURCE"
 
   # Generate stubs and test list based on which symbols were defined
-  printf '\t$(NM) -PA "%s" | awk -f "%s" >"%s"\n' \
-    "$CHECK_OBJECT" \
-    "$BUILD_SOURCE_DIR/check/checkgen.awk" \
-    "$CHECK_GENSRC"
+  build_sh_recipe "\
+\$(NM) -PA \"$CHECK_OBJECT\" | \
+awk -f \"$BUILD_SOURCE_DIR/check/checkgen.awk\" \
+>\"$CHECK_GENSRC\""
 
-  build_source_recipe -c "$CHECK_GENSRC" -o "$CHECK_GENOBJ"
+  build_cc_recipe -c "$CHECK_GENSRC" -o "$CHECK_GENOBJ"
 
-  build_binary_recipe \
-    -o "\"$CHECK_BINARY\"" \
-    "\"$CHECK_OBJECT\"" \
-    "\"$CHECK_GENOBJ\"" \
+  build_ld_recipe \
+    -o "\"$CHECK_BINARY"\" \
+    "\"$CHECK_OBJECT"\" \
+    "\"$CHECK_GENOBJ"\" \
     -lcheck \
-    -l$MODULE \
-    "\$(MODULE_LDFLAGS.$MODULE)"
+    -l$MODULE
 
-  printf '\trm "%s" "%s" "%s" "%s"\n' \
+  build_rm_recipe \
     "$CHECK_OBJECT" \
-    "$CHECK_MKFILE" \
     "$CHECK_GENSRC" \
     "$CHECK_GENOBJ"
 
-  printf '\ntest-%s: %s\n\n' "$MODULE" "$CHECK_BINARY"
-
-  TEST "$CHECK_BINARY"
+  echo
 }
