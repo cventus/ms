7fdd2b68355ba0d77d526267f986e971aca3eb55
Christoffer Ventus <christoffer.ventus@futurice.com>
2018-08-12 20:46:14 +0300

    Keep object files around for simpler builds
    Create archives from object files instead of creating them straight from
    source files. I.e. instead of a single rule like
    
        libfoo.a(bar.o): bar.c
    
    have two rules
    
        bar.o: bar.c
        libfoo.a: bar.o
    
    This implies that object files are kept around in the target directory
    even thought they are (currently) never directly linked against. The
    first build is likely marginally faster since the module library isn't
    expanded after every added object, but subsequent builds might become
    similarly marginally slower since more files are involved.
    
    The main benefit is to allow for parallel builds without locking the
    archive. There is no guarantee that *ar* can handle parallel changes to
    an archive in a sane way, and same with e.g. GNU Make. A minor second
    benefit is that it allows for using an archive format that make does not
    recognize, e.g. in cross-compilation.

--- fixtures/testgen/src/check/lib.sh
+++ fixtures/testgen/src/check/lib.sh
@@ -17,6 +17,7 @@ addcheck()
   CHECK_GENSRC="$TEST_TEMP_DIR/${CHECK_NAME}_check.c"
   CHECK_GENOBJ="$TEST_TEMP_DIR/${CHECK_NAME}_check.o"
   CHECK_BINARY="$TEST_BIN_DIR/$CHECK_NAME"
+  CHECK_DEPEND="$TARGET_DEPEND_DIR/mk-test-$MODULE-$CHECK_NAME.a"
 
   append TESTS "$CHECK_BINARY"
 
@@ -26,20 +27,21 @@ addcheck()
     "$LIBRARY" \
     "$TARGET_LIBRARY_DIR/libcheck.a"
 
-  build_object_recipe \
-    "$CHECK_BINARY" \
-    "$TEST_DEPEND" \
-    "$CHECK_OBJECT" \
-    "$CHECK_SOURCE"
+  build_object_recipe "$CHECK_OBJECT" "$CHECK_SOURCE" "$@"
+
+  # Add source dependencies to archive
+  build_ar_recipe "$CHECK_DEPEND" "${CHECK_OBJECT%.o}.d"
 
   # Generate stubs and test list based on which symbols were defined
-  build_sh_recipe "\
-\$(NM) -PA \"$CHECK_OBJECT\" | \
-awk -f \"$BUILD_SOURCE_DIR/check/checkgen.awk\" \
->\"$CHECK_GENSRC\""
+  build_sh_recipe \
+    '$(NM)' -PA $CHECK_OBJECT \| \
+    awk -f $BUILD_SOURCE_DIR/check/checkgen.awk \
+    \>$CHECK_GENSRC
 
+  # Compile generated source
   build_cc_recipe -c "$CHECK_GENSRC" -o "$CHECK_GENOBJ"
 
+  # Link test executable
   build_ld_recipe \
     -o "\"$CHECK_BINARY"\" \
     "\"$CHECK_OBJECT"\" \
@@ -47,8 +49,10 @@ awk -f \"$BUILD_SOURCE_DIR/check/checkgen.awk\" \
     -lcheck \
     -l$MODULE
 
+  # Remove temporary files
   build_rm_recipe \
     "$CHECK_OBJECT" \
+    "${CHECK_OBJECT%.o}.d" \
     "$CHECK_GENSRC" \
     "$CHECK_GENOBJ"
 
